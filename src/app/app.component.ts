be859ebbe52092b3eb21ab40fe46693c99b3247b
yeida2916 <amadeo565@gmail.com>
2024-10-25 21:14:22 +0200

    componente generator-examen subido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
-import { ExamListComponent } from './pages/exam-list/exam-list.component'; 
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MyComponent } from './components/my-component';
+import { ExamListComponent } from './pages/exam-list/exam-list.component';
+import { UploadFileComponent } from "./pages/generator-examen/upload-file.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule, ExamListComponent, MyComponent],
+  imports: [RouterOutlet, RouterModule, ExamListComponent, MyComponent, UploadFileComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { ExamListComponent } from './pages/exam-list/exam-list.component';
-import { RegisterComponent } from './pages/register/register.component'; // supondremos que tienes este componente
-import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component'; // si lo implementas
 import { AuthGuard } from './core/guards/auth.guard'; // si lo implementas
-
+import { ExamListComponent } from './pages/exam-list/exam-list.component';
+import { UploadFileComponent } from './pages/generator-examen/upload-file.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component'; // si lo implementas
+import { RegisterComponent } from './pages/register/register.component'; // supondremos que tienes este componente
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'recover-password', component: RecoverPasswordComponent },
   { path: 'exam-list', component: ExamListComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'generator-examen',component:UploadFileComponent}
 ];
