cdb287e341a48a8cbf6acd7d7fb97e4f5377a15c
yeida2916 <amadeo565@gmail.com>
2024-10-30 20:41:42 +0100

    ya he subido el dos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { MyComponent } from './components/my-component';
 import { ExamListComponent } from './pages/exam-list/exam-list.component';
 import { ExamUploaderComponent } from "./pages/pantalla-generar-examen/generar-examen.component";
 import { ExcelUploaderComponent } from "./pages/pantalla-validar-archivo-excel/validar-formato.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, ExamListComponent, MyComponent, ExamUploaderComponent, ExcelUploaderComponent],
+  imports: [RouterOutlet, RouterModule, ExamListComponent, MyComponent, ExamUploaderComponent, ExcelUploaderComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   template: `
