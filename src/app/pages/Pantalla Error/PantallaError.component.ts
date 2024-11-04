1a9f4a8924cbc225246be8ef3ca20a6990623969
GabrielH00234 <humaneshugo123@gmail.com>
2024-11-04 14:36:59 +0100

    Add files via upload

--- src/app/pages/Pantalla Error/PantallaError.component.ts
+++ src/app/pages/Pantalla Error/PantallaError.component.ts
@@ -1,7 +1,26 @@
-function closePopup() {
-    alert("Ventana cerrada");
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'Pantalla Error',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent],
+  templateUrl: './Pantalla.Error.Component.html',
+  styleUrls: ['./Pantalla.Error.Component.css' ,]
 }
+
+
+
+
+
+
+
+
 
 function goBack() {
     alert("Volviendo a la página anterior");
 }
+
