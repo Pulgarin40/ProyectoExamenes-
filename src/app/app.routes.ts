b580b42ff5414c94104fe7e43f698547c87a2f69
GabrielH00234 <humaneshugo123@gmail.com>
2024-11-04 14:37:35 +0100

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { ExamListComponent } from './pages/exam-list/exam-list.component';
 import { RegisterComponent } from './pages/register/register.component'; // supondremos que tienes este componente
 import { RecoverPasswordComponent } from './pages/recover-password/recover-password.component'; // si lo implementas
 import { AuthGuard } from './core/guards/auth.guard'; // si lo implementas
+import { TestGeneratorComponent } from './pages/test-generator/test-generator.component';
+import { ExcelReaderComponent } from './pages/excel-reader/excel-reader.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'recover-password', component: RecoverPasswordComponent },
   { path: 'exam-list', component: ExamListComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/exam-list', pathMatch: 'full' },
+  { path: 'test-generator', component: TestGeneratorComponent},
+  { path: 'excel-reader', component: ExcelReaderComponent },
 ];
