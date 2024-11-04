97426b916d9226e39c336e9fd3442713d966a360
GabrielH00234 <humaneshugo123@gmail.com>
2024-11-04 14:53:46 +0100

    Add files via upload

---
+++ src/app/pages/Pantalla Exito/PantallaExito.component.ts
@@ -0,0 +1,47 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'Pantalla Exito',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'PantallaExito.component.html',
+  styleUrls: ['PantallaExito.component.css']
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openPopup() {
+  const popup = document.getElementById("popup");
+  if (popup) {
+    popup.style.display = "block";
+  }
+  const popupOverlay = document.getElementById("popup-overlay");
+  if (popupOverlay) {
+    popupOverlay.style.display = "block";
+  }
+}
+
+function closePopup() {
+  const popup = document.getElementById("popup");
+  if (popup) {
+    popup.style.display = "none";
+  }
+  const popupOverlay = document.getElementById("popup-overlay");
+  if (popupOverlay) {
+    popupOverlay.style.display = "none";
+  }
+}
